39bc86e16509537c7f7cc4b2a4f8d922e0c6b2bd
Aleksandar Drakaliyski <theall3xander@gmail.com>
2023-09-12 21:03:49 +0100

    Updated settings

--- buymecoffee/settings.py
+++ buymecoffee/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-u^slz)df$c-@5q_ys3)s!oayz9s^@n5_4hx183u99#1=pvx&m_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.41.65.7']
 
 
 # Application definition
